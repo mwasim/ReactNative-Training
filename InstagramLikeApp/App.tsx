dd59ced2aca20fcea40944063b2a4b7a2e728fc3
Wasim <mwasimse@gmail.com>
2024-03-19 17:10:40 +0500

    InstagramLikeApp: Initialized, setup and tested initial setup on the Android Emulator.

---
+++ InstagramLikeApp/App.tsx
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
