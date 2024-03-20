704688af426304261e7c75dd3f6231839c6385c3
Wasim <mwasimse@gmail.com>
2024-03-20 13:22:12 +0500

    InstagramLikeApp: React Native Vector Icons and Types Setup and demo

--- InstagramLikeApp/App.tsx
+++ InstagramLikeApp/App.tsx
@@ -1,10 +1,12 @@
 import {Text, View} from 'react-native';
 import colors from './src/theme/colos';
 import fonts from './src/theme/fonts';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <AntDesign name="home" size={30} />
       <Text
         style={{
           color: colors.primary,
