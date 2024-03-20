e09bbd727f75bf24d3a483a3c38ac64cc723e35c
Wasim <mwasimse@gmail.com>
2024-03-20 11:56:16 +0500

    InstagramLikeApp: theme colors and fonts sizes/weights setup.

--- InstagramLikeApp/App.tsx
+++ InstagramLikeApp/App.tsx
@@ -1,9 +1,18 @@
 import {Text, View} from 'react-native';
+import colors from './src/theme/colos';
+import fonts from './src/theme/fonts';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello React Native!</Text>
+      <Text
+        style={{
+          color: colors.primary,
+          fontSize: fonts.size.md,
+          fontWeight: fonts.weight.bold,
+        }}>
+        Hello React Native!
+      </Text>
     </View>
   );
 };
